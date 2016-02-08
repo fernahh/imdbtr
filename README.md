5e9e83f3d3d8a4d3f1bc4b368bbd6918fc4051c8
fernahh <fernahh@gmail.com>
2016-02-08 16:00:22 -0200

    Add readme and fix cli argv

---
+++ README.md
@@ -0,0 +1,3 @@
+# imdbtr
+
+> IMDB on terminal
--- cli.js
+++ cli.js
@@ -1,11 +1,10 @@
 'use strict';
 
-const minimist = require('minimist');
 const imdbtr = require('./src/imdbtr.js');
 
 let run = argv => imdbtr(argv);
 
 exports.run = argv => {
-  let movie = minimist(argv);
-  run(movie._.pop());
+  let movie = argv._.join(" ");
+  run(movie);
 };
