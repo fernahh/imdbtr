8f74b619ce42d3e07ec8fd6dcbf37cc950010e89
fernahh <fernahh@gmail.com>
2016-02-22 08:50:57 -0300

    Fix bug with single title

--- src/helpers.js
+++ src/helpers.js
@@ -2,15 +2,11 @@
 
 const normalizeName = name => {
   const nameList = name.split(' ', 2);
-  let normalizedList = [];
 
-  if (nameList.length > 1) {
-    normalizedList = nameList.map(currName => {
-      return encodeURI(currName);
-    });
-
-    normalizedList = normalizedList.join('+');
-  }
+  let normalizedList = nameList.map(currName => {
+    return encodeURI(currName);
+  });
+  normalizedList = normalizedList.join('+');
 
   return normalizedList.toString();
 };
